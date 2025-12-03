2e8f392b4f10e45ab33d7f2f2dec02f3dc2722e0
rekha kumawat <rekhasumanku@gmail.com>
2025-12-03 23:21:05 +0530

    Add files via upload

---
+++ Java_program/Problem_on_Numbers/Factor.java
@@ -0,0 +1,76 @@
+//////////////////////////////////////////////////////////////////////////////////////////////
+//
+//required header file
+//
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+import java.util.Scanner;
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Class Name :    Logic
+//  Function Name : Factor
+//  Description :   Dispaly the factors of number
+//  Input :         Integer
+//  output :        void
+//  Author :        Rekha Shankarlal Kumawat
+//  Date :          03/12/2025
+//
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+class Logic
+{
+    int iNo = 0;
+    
+    public Logic(int iNo)
+    {
+        this.iNo = iNo;
+    }
+
+    void Factor()
+    {
+        int iCnt = 0;
+        
+        for(iCnt = 1 ; iCnt <= iNo ; iCnt++)
+        {
+            if((iNo % iCnt) == 0 )
+            {
+                System.out.print(iCnt+"\t");
+            }
+        }
+    
+    }
+    
+}//end of the Logic class
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//
+//Entery point class
+//
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+class Factor
+{
+    public static void main(String Arr[])
+        {
+            int iValue = 0 ;
+
+            Scanner sobj = new Scanner(System.in);
+        
+            System.out.println("Enter first number:");
+            iValue = sobj.nextInt();
+
+            Logic lobj = new Logic(iValue);
+            lobj.Factor();
+
+            
+        }
+
+}//end of main class
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Input1:6                   Output:1  2  3  4  6
+//  Input1:12                  Output:1  2  3  4  6  12
+//
+//////////////////////////////////////////////////////////////////////////////////////////////
