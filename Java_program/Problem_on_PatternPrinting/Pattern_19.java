cd85e8dd5fcae0ddc359c131ab9aa246337f37f3
rekha kumawat <rekhasumanku@gmail.com>
2025-12-19 12:01:38 -0500

    Add files via upload

---
+++ Java_program/Problem_on_PatternPrinting/Pattern_19.java
@@ -0,0 +1,99 @@
+//////////////////////////////////////////////////////////////////////////////////////////////
+//
+//required header file
+//
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+import java.util.Scanner;
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Class Name :    Pattern
+//  Function Name : pattern
+//  Description :   Display pattern
+//  Input :         Integer 
+//  output :        void
+//  Author :        Rekha Shankarlal Kumawat
+//  Date :          11/12/2025
+//
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+class Pattern
+{
+    int iRow ;
+    int icol ;
+
+    public Pattern(int iNo1 , int iNo2)
+    {
+        this.iRow = iNo1 ;
+        this.icol = iNo2 ;
+    }
+
+    public void pattern()
+    {
+        int iCnt1 = 0 ;
+        int iCnt2 = 0 ;
+
+        if(iRow != icol)
+        {
+            System.out.println("Number of Row and Columns should be same");
+            return  ;
+
+        }
+        for(iCnt1 = 1 ; iCnt1 <= iRow ; iCnt1++ )
+        {
+            for(iCnt2 = 1; iCnt2 <= icol ; iCnt2++ )
+            {
+                if(iCnt1 == 1 || iCnt1 == iRow || iCnt2 == 1 || iCnt2 == icol)
+                {
+                    System.out.print("*\t");
+                }
+                else
+                {
+                    System.out.print(("@\t"));
+                }
+                
+            }
+            System.out.print("\n");
+        }
+    }
+}//end of Pattern class
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//
+//Entery point class
+//
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+class Pattern_19
+{
+    public static void main(String A[])
+    {
+        int iValue1 = 0 ;
+        int iValue2 = 0 ;
+
+        Scanner sobj = new Scanner(System.in);
+
+        System.out.println("Enter the number of Rows:");
+        iValue1 = sobj.nextInt();
+
+        System.out.println("Enter the number of Columns:");
+        iValue2 = sobj.nextInt();
+
+        Pattern pobj = new Pattern(iValue1 , iValue2);
+        pobj.pattern();
+    }
+}// end of main class
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Input1:4          Input2:4                      Output: *       *       *       *
+//                                                          *       @       @       *
+//                                                          *       @       @       *
+//                                                          *       *       *       *
+//
+//  Input1:3          Input2:3                       Output:  *       *       *
+//                                                            *       $       *
+//                                                            *       *       *
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
