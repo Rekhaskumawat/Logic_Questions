b584faeccded28174e868c51c58f87dd515568e3
rekha kumawat <rekhasumanku@gmail.com>
2025-12-19 12:00:15 -0500

    Add files via upload

---
+++ C++_program/Problem_on_PatternPrinting/Pattern_17.cpp
@@ -0,0 +1,97 @@
+//////////////////////////////////////////////////////////////////////////////////////////////
+//
+//required header file
+//
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<iostream>
+using namespace std;
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Class Name :    Pattern
+//  Function Name : pattern
+//  Description :   Display pattern
+//  Input :         Integer 
+//  output :        void
+//  Author :        Rekha Shankarlal Kumawat
+//  Date :          19/12/2025
+//
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+class Pattern
+{
+    public:
+
+    int iRow ;
+    int icol ;
+
+    Pattern(int iNo1 , int iNo2)
+    {
+        this->iRow = iNo1 ;
+        this->icol = iNo2 ;
+    } 
+
+    void pattern()
+    {
+        if(iRow != icol)
+        {
+            cout<<"Number of Row and Columns should be same";
+            return ;
+        }
+
+        int iCnt1 = 0 ;
+        int iCnt2 = 0 ;
+
+        for(iCnt1 = 1; iCnt1 <= iRow ; iCnt1++ )
+        {
+            for(iCnt2 = 1 ; iCnt2 <= icol ; iCnt2++ )
+            {
+                if(iCnt1 >= iCnt2)
+                {
+                    cout<<"*\t";
+                }
+                else
+                {
+                    cout<<"#\t";
+                }
+            }
+            cout<<"\n";
+        }
+    }
+};//end of Pattern class
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+//
+//Entery point function
+//
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0 ;
+    int iValue2 = 0 ;
+
+    cout<<"Enter the number of Rows:";
+    cin>>iValue1;
+
+    cout<<"Enter the number of Columns:";
+    cin>>iValue2;
+
+    Pattern pobj(iValue1 , iValue2);
+    pobj.pattern();
+
+}// end of main function
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Input1:4          Input2:4                      Output: *       #       #       #
+//                                                          *       *       #       #
+//                                                          *       *       *       #
+//                                                          *       *       *       *
+//
+//  Input1:3          Input2:3                       Output:  *       #       #
+//                                                            *       *       #
+//                                                            *       *       *
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
