c28d3c9d7ccb857b37d22c69a92c7944d2a3e66d
rekha kumawat <rekhasumanku@gmail.com>
2025-12-20 12:34:12 -0500

    Add files via upload

---
+++ C++_program/Problem_on_PatternPrinting/Pattern_21.cpp
@@ -0,0 +1,91 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//required header file
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<iostream>                                              // for input output
+using namespace std;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Display
+//  Description :   Display the pattern of character
+//  Input :         Integer
+//  output :        Void
+//  Author :        Rekha Shankarlal Kumawat
+//  Date :          19/12/2025
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Pattern
+{
+    public:
+
+    int iRow ;
+    int icol ;
+
+    Pattern(int iNo1 , int iNo2)
+    {
+        this->iRow = iNo1 ;
+        this->icol = iNo2 ;
+    }
+
+    void pattern()
+    {
+    int iCnt1 = 0 ;
+    int iCnt2 = 0 ;
+    if(iRow != iCol)
+    {
+        printf("Invalid input");
+        printf("Row number and column number should be same");
+        return;
+    }
+    for(iCnt1 = iRow ; iCnt1 >= 1 ; iCnt1--)
+    {
+        
+        for(iCnt2 = 1; iCnt1 >= iCnt2 ; iCnt2++)
+        {
+            
+            cout<<"*\t";
+            
+        }
+        cout<<"\n";
+    }
+}
+};//end of the pattern class
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//Entery point function
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0 ;
+    int iValue2 = 0 ;
+
+    cout<<"Enter the number of Rows:";
+    cin>>iValue1;
+
+    cout<<"Enter the number of Columns:";
+    cin>>iValue2;
+
+    Pattern pobj(iValue1 , iValue2);
+    pobj.pattern();
+    
+}// end of main function
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Input1:4    Input2:4   Output:*       *       *       *
+//                                *       *       *       
+//                                *       *              
+//                                *                     
+//
+//  Input1:3    Input2:3   Output:*       *       *
+//                                *       *       
+//                                *              
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
---
+++ C++_program/Problem_on_PatternPrinting/Pattern_24.cpp
@@ -0,0 +1,94 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//required header file
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include<iostream>                                              // for input output
+using namespace std;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name : Display
+//  Description :   Display the pattern of character
+//  Input :         Integer
+//  output :        Void
+//  Author :        Rekha Shankarlal Kumawat
+//  Date :          19/12/2025
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Pattern
+{
+    public:
+
+    int iRow ;
+    int icol ;
+
+    Pattern(int iNo1 , int iNo2)
+    {
+        this->iRow = iNo1 ;
+        this->icol = iNo2 ;
+    }
+
+    void pattern()
+    {
+    int iCnt1 = 0 ;
+    int iCnt2 = 0 ;
+    if(iRow != iCol)
+    {
+        printf("Invalid input");
+        printf("Row number and column number should be same");
+        return;
+    }
+    for(iCnt1 = 1 ; iCnt1 <= iRow ; iCnt1++)
+    {
+        for(iCnt2 = 1; iCnt2 <= icol ; iCnt2++)
+        {
+            if(iCnt2 < iCnt1)
+            {
+                cout<<"\t";
+            } 
+            else
+            {
+                cout<<"%d"<<iCnt2<<"\t";
+            }
+        }
+        cout<<"\n";
+    }
+}
+};//end of the Display function
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//Entery point function
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    int iValue1 = 0 ;
+    int iValue2 = 0 ;
+
+    cout<<"Enter the number of Rows:";
+    cin>>iValue1;
+
+    cout<<"Enter the number of Columns:";
+    cin>>iValue2;
+
+    Pattern pobj(iValue1 , iValue2);
+    pobj.pattern();
+    
+}// end of main function
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Input1:4    Input2:4   Output:1       2       3       4
+//                                        2       3       4
+//                                                3       4
+//                                                        4
+//
+//  Input1:3    Input2:3   Output:1       2       3
+//                                        2       3
+//                                                3
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
